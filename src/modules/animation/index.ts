36f15dd4854530efe0cf3617edd3989d4f42fbc0
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-07 21:52:12 +0200

    feat: Export animation components and store functions from the new animation module

---
+++ src/modules/animation/index.ts
@@ -0,0 +1,5 @@
+export { ClipImport } from './components/clip-import';
+export { ClipSelector } from './components/clip-selector';
+export { PlaybackControls } from './components/playback-controls';
+export { TimelineScrubber } from './components/timeline-scrubber';
+export { $clips, importClipFiles, pause, play, selectClip, stop, toggleLoop } from './stores/clip-store';
