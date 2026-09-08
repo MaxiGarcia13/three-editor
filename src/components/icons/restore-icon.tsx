dec8406ab7bf577aed473341b56c5d698344b635
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-08 16:08:19 +0200

    feat: Add RestoreIcon component for enhanced iconography in the application

---
+++ src/components/icons/restore-icon.tsx
@@ -0,0 +1,23 @@
+import type { IconProps } from './type';
+import { ICON_SIZE } from './constants';
+
+export function RestoreIcon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={ICON_SIZE}
+      height={ICON_SIZE}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M9 14l-4 -4l4 -4" />
+      <path d="M5 10h11a4 4 0 1 1 0 8h-1" />
+    </svg>
+  );
+}
