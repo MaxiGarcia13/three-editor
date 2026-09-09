6ca0e2e3e9f49575bebe8b9039e848b7fcf783af
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-09 15:32:27 +0200

    fix: Update axesVisible setting in viewport settings store to false for improved default behavior

--- src/modules/viewport/stores/viewport-settings-store.ts
+++ src/modules/viewport/stores/viewport-settings-store.ts
@@ -10,7 +10,7 @@ export interface ViewportSettingsState {
 }
 
 export const $viewportSettings = map<ViewportSettingsState>({
-  axesVisible: true,
+  axesVisible: false,
   axesSize: AXES_SIZE,
 });
 
