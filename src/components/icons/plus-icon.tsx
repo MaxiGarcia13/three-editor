4e7b49792063179b751273ae0cced78c42511538
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-09 17:12:09 +0200

    feat: Add PlusIcon component for SVG icon representation; includes customizable properties and adheres to design specifications

---
+++ src/components/icons/plus-icon.tsx
@@ -0,0 +1,23 @@
+import type { IconProps } from './type';
+import { ICON_SIZE } from './constants';
+
+export function PlusIcon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={ICON_SIZE}
+      height={ICON_SIZE}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M12 5l0 14" />
+      <path d="M5 12l14 0" />
+    </svg>
+  );
+}
