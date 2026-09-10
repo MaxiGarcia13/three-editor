f8658d44c3e73f770af683e88d496d1a4eb61c30
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-10 18:33:30 +0200

    feat: Export useCollapsible hook from collapsible component for improved context management

--- src/components/collapsible/index.ts
+++ src/components/collapsible/index.ts
@@ -1,3 +1,4 @@
 export { Collapsible } from './collapsible';
 export { CollapsibleContent } from './collapsible-content';
+export { useCollapsible } from './collapsible-context';
 export { CollapsibleHeader } from './collapsible-header';
