8d7628ff7546b44accc8c6e81ad0ad893a840383
Maximiliano Garcia Mortigliengo <maxig8@hotmail.com>
2026-09-10 17:04:04 +0200

    feat: Add EyeIcon and EyeOffIcon components for enhanced visibility control in UI

---
+++ src/components/icons/eye-icon.tsx
@@ -0,0 +1,23 @@
+import type { IconProps } from './type';
+import { ICON_SIZE } from './constants';
+
+export function EyeIcon(props: IconProps) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={ICON_SIZE}
+      height={ICON_SIZE}
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      {...props}
+    >
+      <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+      <path d="M10 12a2 2 0 1 0 4 0a2 2 0 0 0 -4 0" />
+      <path d="M21 12c-2.4 4 -5.4 6 -9 6c-3.6 0 -6.6 -2 -9 -6c2.4 -4 5.4 -6 9 -6c3.6 0 6.6 2 9 6" />
+    </svg>
+  );
+}
